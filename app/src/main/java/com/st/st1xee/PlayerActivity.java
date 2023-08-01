952927727cab0c106912e13d5ebd4d8a3f39f63b
Misha Galagan <misha.galagan2@gmail.com>
2023-08-01 12:18:30 +0300

    first commit andr

---
+++ app/src/main/java/com/st/st1xee/PlayerActivity.java
@@ -0,0 +1,14 @@
+package com.st.st1xee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
